d88c7716a5d5e46f0d74027107252ee7cfac9dbc
HENRY_PC\leema <leemanhin1997@gmail.com>
2024-10-28 08:12:04 -0400

    Adding deploy settings

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # build
-npm run build
+pnpm run build
 
 # navigate into the build output directory
 cd dist
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:manhin1997/henry_lee_portfolio.git main:gh-pages
 
 cd -
